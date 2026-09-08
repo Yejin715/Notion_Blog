79197372b13ca688a16ca7c8299f68e8a42e23a7
YejinChoi <715jin@naver.com>
2026-09-08 13:51:15 +0900

    fix: social images

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,11 +1,5 @@
 import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
-  apiBaseUrl: process.env.NOTION_API_BASE_URL,
-  kyOptions: {
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/126.0.0.0 Safari/537.36'
-    }
-  }
+  apiBaseUrl: process.env.NOTION_API_BASE_URL
 })
