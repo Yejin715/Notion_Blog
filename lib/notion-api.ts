d9a33584bf84d316f5e95a3ca0c50f14cee574c3
YejinChoi <715jin@naver.com>
2026-09-08 13:41:14 +0900

    Change ofetchOptions to kyOptions in NotionAPI

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -2,7 +2,7 @@ import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
   apiBaseUrl: process.env.NOTION_API_BASE_URL,
-  ofetchOptions: {
+  kyOptions: {
     headers: {
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/126.0.0.0 Safari/537.36'
