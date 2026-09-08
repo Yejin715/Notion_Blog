2c1cfd3401aa5d90917c1c0863115bf71ee08e2d
YejinChoi <715jin@naver.com>
2026-09-08 13:55:17 +0900

    Add kyOptions with User-Agent to NotionAPI

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,5 +1,11 @@
 import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
-  apiBaseUrl: process.env.NOTION_API_BASE_URL
+  apiBaseUrl: process.env.NOTION_API_BASE_URL,
+  kyOptions: {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/126.0.0.0 Safari/537.36'
+    }
+  }
 })
