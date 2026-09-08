2dd8075a7d2949b2ca83a9be733c696e81f96292
YejinChoi <715jin@naver.com>
2026-09-08 15:18:43 +0900

    Refactor getStaticProps and getStaticPaths logic

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -1,22 +1,20 @@
 import { type GetStaticProps } from 'next'
 
 import { NotionPage } from '@/components/NotionPage'
-import { domain } from '@/lib/config'
+import { domain, isDev } from '@/lib/config'
+import { getSiteMap } from '@/lib/get-site-map'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
 import { type PageProps, type Params } from '@/lib/types'
 
 export const getStaticProps: GetStaticProps<PageProps, Params> = async (
   context
 ) => {
-  const rawPageId = context.params?.pageId as string
+  const rawPageId = context.params.pageId as string
 
   try {
     const props = await resolveNotionPage(domain, rawPageId)
 
-    return {
-      props,
-      revalidate: 10
-    }
+    return { props, revalidate: 10 }
   } catch (err) {
     console.error('page error', domain, rawPageId, err)
 
@@ -25,12 +23,28 @@ export const getStaticProps: GetStaticProps<PageProps, Params> = async (
 }
 
 export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: 'blocking'
+  if (isDev) {
+    return {
+      paths: [],
+      fallback: true
+    }
   }
+
+  const siteMap = await getSiteMap()
+
+  const staticPaths = {
+    paths: Object.keys(siteMap.canonicalPageMap).map((pageId) => ({
+      params: {
+        pageId
+      }
+    })),
+    fallback: true
+  }
+
+  console.log(staticPaths.paths)
+  return staticPaths
 }
 
-export default function NotionDomainDynamicPage(props: PageProps) {
+export default function NotionDomainDynamicPage(props) {
   return <NotionPage {...props} />
 }
